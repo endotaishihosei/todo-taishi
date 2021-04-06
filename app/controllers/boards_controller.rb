e880cf2d1b75ecf8a8afccdb1ccb00c50461f91f
遠藤大志 <taishi.domybest3607@gmail.com>
2021-04-06 22:27:56 +0900

    new action create action strong parameter実行

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,25 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
   end
+
+  def new
+    @board = Board.new 
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board), notice: '保存に成功'
+    else
+      flash.now[:error] = '保存に失敗'
+      render :new
+    end
+
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:title, :name, :content)
+    end
+
 end
