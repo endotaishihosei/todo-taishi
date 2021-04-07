f9eed45da1cff928229a86af1d6f6ecf10519749
遠藤大志 <taishi.domybest3607@gmail.com>
2021-04-07 22:44:51 +0900

    validationの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,7 @@
 class Board < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3}
+  validates :name, presence: true, length: {minimum: 3}
+  validates :content, presence: true
+
+
 end
