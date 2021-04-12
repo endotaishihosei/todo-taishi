022cf5e8790bea5e92d5d3e0fe0194e4e0bf572e
遠藤大志 <taishi.domybest3607@gmail.com>
2021-04-13 00:17:50 +0900

    独自のvalidationを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -18,8 +18,19 @@ class Board < ApplicationRecord
   validates :content, presence: true
   validates :content, uniqueness: true
 
+  validate :validate_title_and_content_length
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
+
+  private 
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    if char_count < 30
+      errors.add(:content, '30文字以上で！')
+    end
+  end
+
 
 end
