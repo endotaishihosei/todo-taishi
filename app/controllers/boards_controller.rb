26e0485276c9ffd0fe4d8a0874d8e9e2593c6aae
遠藤大志 <taishi.domybest3607@gmail.com>
2021-04-12 14:01:35 +0900

    before_actionその2

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,5 @@
 class BoardsController < ApplicationController
-  before_action :set_article
+  before_action :set_board, only: [:show, :edit, :update]
 
   def index
     @boards = Board.all
@@ -27,12 +27,28 @@ class BoardsController < ApplicationController
     # new actionとの違い => editはidが渡ってくる
   end
 
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@article), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
+  end
+
   private
     def board_params
       params.require(:board).permit(:title, :name, :content)
     end
 
-    def set_article
+    def set_board
       @board = Board.find(params[:id])
     end
 
